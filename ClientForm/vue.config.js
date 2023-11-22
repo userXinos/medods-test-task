c223db6d63814a117f67dafab9e748f6fbf60f4a
Xinos <userxinos@gmail.com>
2023-11-23 01:53:26 +0700

    set public path

--- ClientForm/vue.config.js
+++ ClientForm/vue.config.js
@@ -3,5 +3,6 @@ const {defineConfig} = require('@vue/cli-service');
 
 module.exports = defineConfig({
     transpileDependencies: true,
+    publicPath: `/medods-test-task/ClientForm/`,
     outputDir: '../dist/ClientForm/',
 });
